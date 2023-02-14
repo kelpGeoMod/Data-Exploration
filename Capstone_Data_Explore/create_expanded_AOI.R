00b14641ae0e9d5251869864039e1fbff11b3726
Jessica French <jessica.c.french6@gmail.com>
2023-02-13 16:19:39 -0800

    added map of distribution on nutrient data so far

--- Capstone_Data_Explore/create_expanded_AOI.R
+++ Capstone_Data_Explore/create_expanded_AOI.R
@@ -1,17 +1,17 @@
-# Create A Shapefile of our area of interest -120.50, 33.83, -119.45, 34.49
+# Create A Shapefile of our area of expanded area interest -123.32, 30.59, -115.70, 36.08
 #Load in libraries
 library(tidyverse)
 library(sf)
-library(tmap)
+
 
 # Create a data frame from the coordinates
 
 coordinates <- tribble(
   ~lat, ~lon,
-  33.83, -120.50, 
-  33.83, -119.45, 
-  34.49, -120.50, 
-  34.49, -119.45
+  30.59, -123.32, 
+  30.59, -115.70, 
+  36.08, -123.32, 
+  36.08, -115.70
 ) |> 
   st_as_sf(coords = c("lon", "lat"), 
            crs = "EPSG: 4326") |> 
@@ -21,5 +21,8 @@ coordinates <- tribble(
 
 # change your file path to wherever you want to save the shape file.
 st_write(coordinates,
-         "/Users/jfrench/Documents/MEDS/Capstone/DATA/AOI_SBchannel.shp", 
+         "/Users/jfrench/Documents/MEDS/Capstone/DATA/Expaned_AOI_SBchannel.shp", 
          driver = "ESRI Shapefile")
+
+
+
